6ed1629efc60a5487f3e2702c110da0b14fcb0f9
paislee <caleb.sotelo@paislee.net>
2012-12-03 17:52:26 -0800

    v2 repeat group simple working

--- minlate.js
+++ minlate.js
@@ -18,77 +18,135 @@ var minlate = function(document) {
     var cssHide = ".minlate{display:none !important;}",
         cssEl = document.createElement("style"),
         mem = {}, // store used templates outside of DOM
-        render = function(template, values, startChar, endChar) {
+        render = function(template, values, startChar, endChar, groupStartChar, groupEndChar, escChar) {
           
           var buf = "",
               bufOpen = 0,
+              grpStartIndex,// index of the beginning of current group
+              grpCount = -1,// number of times through current group, group closed if < zero
               result = "",
               currentChar = "",
-              values = values || {};
+              values = values || {},
+              value,
+              arrLength;
           
-          // process template char by char, look for tokens
+          // process template char by char
           for (var i = 0; i < template.length; i++) {
-            currentChar = template.charAt(i);
+            currentChar = template.charAt(i);            
             switch (currentChar) {
-              case startChar:
+              case startChar:// start capturing a value                
                 bufOpen = 1;
                 break;
-              case endChar:
-                result += values[buf] || "";
-                bufOpen = 0;
+              case endChar:// done capturing a value
+                value = values[buf] || "";
+                if (grpCount+1 && value instanceof Array) {// in a group and value is an array
+                   arrLength = value.length;// save the length of value array
+                   if (arrLength) value = value[grpCount];// if the array has a length, get the value
+                   if (grpCount >= arrLength-1) grpCount = -1;// no more elements, mark group exhausted
+                }
+                result += value;// add retrieved value to end result
+                bufOpen = 0;// clear the value buffer
                 buf = "";
                 break;
+              case groupStartChar:
+                if (grpCount == -1) grpCount++;
+                grpStartIndex = i;
+                break;
+              case groupEndChar:
+                if (grpCount+1) {// groupCount was not set to -1
+                  i = grpStartIndex - 1;// start group again
+                  grpCount++;
+                }// otherwise this group is done..
+                break;
+              case escChar:
+                currentChar = template.charAt(++i);// jump ahead one
               default:
-                if (bufOpen) {
-                  buf += currentChar;
-                } else {
-                  result += currentChar;
-                }
+                if (bufOpen) buf += currentChar;
+                else result += currentChar;                
              }
           }
           return result;
-        },
-        initCSS = function() {
-          // add css rule to hide all templates
-          cssEl.type = "text/css";
-          if (cssEl.styleSheet) {
-              cssEl.styleSheet.cssText = cssHide;
-          } else {
-              cssEl.appendChild(document.createTextNode(cssHide));
-          }
-          document.getElementsByTagName("head")[0].appendChild(cssEl);      
         };
-    
-        initCSS();
+        
+        cssEl.type = "text/css";
+        if (cssEl.styleSheet) {
+            cssEl.styleSheet.cssText = cssHide;
+        } else {
+            cssEl.appendChild(document.createTextNode(cssHide));
+        }
+        document.getElementsByTagName("head")[0].appendChild(cssEl);
     
     // public interface
-    return function(options) {
+    // opts:
+    //   templateString
+    //   id
+    //   values
+    //   inPlace
+    //   startChar
+    //   endChar
+    //   groupStartChar
+    //   groupEndChar
+    //   escapeChar
+    return function(opts) {
       
       var inDOMNode,
           sourceNode,
           copyNode,
-          values = options.values || {},
-          savedTemplateNodes,
-          startChar = options.startChar || "{",
-          endChar = options.endChar || "}";
-          
-      if (options.id) {// render a template specified in the DOM
-        inDOMNode = document.getElementById(options.id);
-        sourceNode = savedTemplateNodes[options.id] || inDOMNode;
-        if (sourceNode && sourceNode.className.indexOf(/\bminlate\b/) !== -1) {
+          templateStr       = opts.templateString,
+          values            = opts.values || {},
+          savedNodes        = {},
+          startChar         = opts.startChar || "{",
+          endChar           = opts.endChar || "}",
+          groupStartChar    = opts.groupStartChar || "[",
+          groupEndChar      = opts.groupEndChar || "]",
+          escChar           = opts.escapeChar || "\\",
+          specialCharMap    = {},
+          specialCharCount  = 0;
+      
+      // make sure special characters are unique
+      specialCharMap[startChar]       = ++specialCharCount;
+      specialCharMap[endChar]         = ++specialCharCount;
+      specialCharMap[groupStartChar]  = ++specialCharCount;
+      specialCharMap[groupEndChar]    = ++specialCharCount;
+      specialCharMap[escChar]         = ++specialCharCount;      
+      for (var specialChar in specialCharMap) {
+        specialCharMap.hasOwnProperty(specialChar) && specialCharCount--;
+      }
+      if (specialCharCount) throw new Error("Special characters must be unique.");
+      
+      if (opts.id) {// render a template specified in the DOM
+        inDOMNode = document.getElementById(opts.id);
+        sourceNode = savedNodes[opts.id] || inDOMNode;
+        if (sourceNode && sourceNode.className.match(/\bminlate\b/)) {
           copyNode = sourceNode.cloneNode();
-          copyNode.innerHTML = render(sourceNode.innerHTML, values, startChar, endChar);
-          copyNode.className = copy.className.replace(/\bminlate\b/,"");
-          savedTemplateNodes[options.id] = sourceNode;
-          inDOMNode.parentNode.replaceChild(copyNode, inDOMNode);
+          copyNode.innerHTML = render(
+            sourceNode.innerHTML,
+            values,
+            startChar,
+            endChar,
+            groupStartChar,
+            groupEndChar,
+            escChar
+          );
+          copyNode.className = copyNode.className.replace(/\bminlate\b/,"");
+          savedNodes[opts.id] = sourceNode;
+          opts.inPlace && inDOMNode.parentNode.replaceChild(copyNode, inDOMNode);
           return copyNode;
         }
-        throw "Invalid template element";
+        throw new Error("Invalid template element.");
       } else {// render a template provided as a string
-        if (options.template && (typeof options.template === "string")) {
-          return render(options.template, values, startChar, endChar);
+        if (typeof templateStr === "string") {
+          return render(
+            templateStr,
+            values,
+            startChar,
+            endChar,
+            groupStartChar,
+            groupEndChar,
+            escChar
+          );
         }
-        throw "Invalid template string";
+        throw new Error("Invalid template string.");
       }
     };
      
--- tests/qunit/minlate.tests.js
+++ tests/qunit/minlate.tests.js
@@ -2,14 +2,14 @@
   
   module("Minlate-js Tests");
   
-  test("Simple minlate test, template provided", function() {
+  test("Render simple template out of DOM", function() {
     
     var template = "<p>Hello {key}!</p>",
         expected = "<p>Hello World!</p>",
         actual;
         
     actual = minlate({
-      template : template,
+      templateString : template,
       values : {
         key : "World"
       }
@@ -19,23 +19,85 @@
     
   });
   
-  test("Test overriding replacement symbols", function() {
+  test("Render larger template out of DOM", function() {
     
-    var template = "<p>Hello [key]!</p>",
+      var template = "Lorem {one} dolor sit amet, {two} adipiscing elit. Mauris ultricies {three} turpis, viverra pharetra sem {four} non. Donec a mi vel orci luctus {five} nec nec quam. Morbi facilisis tempor odio in {six}. Sed gravida viverra {seven}. Vestibulum condimentum ligula a eros {eight} dapibus non a quam. Integer {nine} commodo ornare. Aenean urna nibh, adipiscing ac auctor {ten}, mollis a velit. Nam non diam non mauris {eleven} vestibulum eu sit amet leo. {twelve} potenti. Nulla facilisi.",
+          expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultricies auctor turpis, viverra pharetra sem posuere non. Donec a mi vel orci luctus facilisis nec nec quam. Morbi facilisis tempor odio in ultricies. Sed gravida viverra sollicitudin. Vestibulum condimentum ligula a eros fermentum dapibus non a quam. Integer vestibulum commodo ornare. Aenean urna nibh, adipiscing ac auctor dictum, mollis a velit. Nam non diam non mauris vehicula vestibulum eu sit amet leo. Suspendisse potenti. Nulla facilisi.",          
+          actual;
+
+      actual = minlate({
+        templateString : template,
+        values : {
+          one : "ipsum",
+          two : "consectetur",
+          three : "auctor",
+          four : "posuere",
+          five : "facilisis",
+          six : "ultricies",
+          seven : "sollicitudin",
+          eight : "fermentum",
+          nine : "vestibulum",
+          ten : "dictum",
+          eleven : "vehicula",
+          twelve : "Suspendisse"
+        }
+      });
+      
+      equal(actual, expected, "Expect correctly rendered template.");
+    });
+  
+  test("Render simple template out of DOM, override replacement symbols", function() {
+    
+    var template = "<p>Hello $key%!</p>",
         expected = "<p>Hello World!</p>",
         actual;
         
     actual = minlate({
-      template : template,
+      templateString : template,
       values : {
         key : "World"
       },
-      startChar : "[",
-      endChar : "]"
+      startChar : "$",
+      endChar : "%"
     });
     
     equal(actual, expected, "Expect correctly rendered template.");
      
+  });
+  
+  test("Render repeat template out of DOM", function() {
+    
+    var template = "Hello test[, {number}]!",
+        expected = "Hello test, one, two, three!",
+        actual;
+        
+    actual = minlate({
+      templateString : template,
+      values : {
+        number : ["one", "two", "three"]
+      }
+    });
+    
+    equal(actual, expected, "Expect correctly rendered template.");
+    
+  });
+  
+  test("Render repeat template, multiple values", function() {
+    
+    var template = "Hello test[, {numbers} AND {colors}]!",
+        expected = "Hello test, one AND red, two AND orange, three AND yellow!",
+        actual;
+        
+    actual = minlate({
+      templateString : template,
+      values : {
+        colors : ["red", "orange", "yellow"],
+        numbers : ["one", "two", "three"]
+      }
+    });
+    
+    equal(actual, expected, "Expect correctly rendered template.");
+    
   });
 
 })();
